5231f4deb8f625ac22e85c450a7bd2fb00710244
Chanmann Lim <chanmannlim@gmail.com>
2024-02-29 22:18:41 -0500

    TermOpen autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+--  https://www.youtube.com/watch?v=qN6BuJpsFbQ
+vim.api.nvim_create_augroup("custom_buffer", { clear = true })
+
+-- open terminal with :term
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = "custom_buffer",
+  pattern = "*",
+  command = "startinsert | set winfixheight"
+})
+
